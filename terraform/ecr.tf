bce4d03799ccfd63bd1ca835b6e9b87df7187e96
Abrar Saiyyad <131968969+Abrarsaiyyad8@users.noreply.github.com>
2026-02-17 19:22:25 +0530

    Update ECR repositories for frontend and backend

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,11 +1,23 @@
 resource "aws_ecr_repository" "game_repo" {
-  name = "browser-game"
+  name = "browser-game-frontend"
 
   image_scanning_configuration {
     scan_on_push = true
   }
 
   tags = {
-    Name = "browser-game-repo"
+    Name = "browser-game-frontend"
+  }
+}
+
+resource "aws_ecr_repository" "game_repo_secound" {
+  name = "browser-game-backend"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "browser-game-backend"
   }
 }
